c99c1556cb7e239dccc699a967bd1749ab8a4043
HeeChul So <heechuls99@gmail.com>
2019-02-10 15:34:30 +0900

    initialize

---
+++ src/main/kotlin/com/example/springredis/SpringRedisApplication.kt
@@ -0,0 +1,12 @@
+package com.example.springredis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRedisApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringRedisApplication>(*args)
+}
+
